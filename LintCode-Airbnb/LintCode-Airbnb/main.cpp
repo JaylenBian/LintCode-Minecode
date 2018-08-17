7fcd0d70d84d5925df730f05b834dfd6d6988426
Minecode <minecoder@163.com>
2018-08-17 22:07:34 +0800

    Configure Project
    Change-Id: Ie60bf8ebfc354a8633a917df32052d0a7c91de1c

---
+++ LintCode-Airbnb/LintCode-Airbnb/main.cpp
@@ -0,0 +1,52 @@
+//
+//  main.cpp
+//  LintCode-Airbnb
+//
+//  Created by 边俊林 on 2018/8/15.
+//  Copyright © 2018 minecode. All rights reserved.
+//
+
+/* ------------------------------------------------- *\
+ https://www.lintcode.com/problem/two-sum/description
+\* ------------------------------------------------- */
+
+#include <map>
+#include <set>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+
+/// Tips:
+//
+
+// Tool Function List
+void printVector(vector<int> v) {
+    printf("\n[");
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        printf("%c%d", it==v.begin() ? '\0' : ' ', *it);
+    }
+    printf("]\n");
+}
+void printVectors(vector<vector<int>> v) {
+    printf("\n[");
+    for (auto it = v.begin(); it != v.end(); ++it) {
+        printVector(*it);
+    }
+    printf("]\n");
+}
+
+int main() {
+    
+    return 0;
+}
