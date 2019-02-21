8787c9a9ddaff624a266c993d7475fb47eb3d8e8
Minecode <Minecoder@163.com>
2019-02-21 12:43:54 +0800

    LintCode - Google initialize
    Change-Id: Ia7e121a70f4958f1c6f9b86be47a6aa5b0ef0efd

---
+++ LintCode-Google/LintCode-Google/main.cpp
@@ -0,0 +1,43 @@
+//
+//  main.cpp
+//  LintCode-Google
+//
+//  Created by 边俊林 on 2019/2/21.
+//  Copyright © 2019 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+    
+    
+private:
+    
+};
+
+/// Tips:
+//
+
+
+int main() {
+    
+    Solution sol = Solution();
+    
+    return 0;
+}
