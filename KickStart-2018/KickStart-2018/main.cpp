53738a9606e4cdefbfbfcef12f482a9f11d31152
Minecode <Minecoder@163.com>
2019-03-05 13:14:51 +0800

    KickStart-2018. Initialize
    Change-Id: Iee5ead3267a6b12e48bd92900bf265799feed536

---
+++ KickStart-2018/KickStart-2018/main.cpp
@@ -0,0 +1,42 @@
+//
+//  main.cpp
+//  KickStart-2018
+//
+//  Created by 边俊林 on 2019/3/5.
+//  Copyright © 2019 边俊林. All rights reserved.
+//
+
+#include <map>
+#include <set>
+#include <list>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+/// Solution:
+//
+class Solution {
+public:
+
+
+private:
+
+};
+
+/// Tips:
+//
+
+int main() {
+
+    Solution sol = Solution();
+
+    return 0;
+}
